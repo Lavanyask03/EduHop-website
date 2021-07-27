7d8d2c9f95144a939d7d6438d930e0798f5ade4d
Lavanya <lavanyask03@gmail.com>
2021-07-27 16:47:19 +0530

    login modal pop up

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ const body = document.querySelector('body');
 hamburger.addEventListener("click", () => {
     body.classList.toggle('show');
 })
+
+// login modal
+const loginBtn = document.getElementById('login');
+const closeBtn = document.getElementById('close');
+const modalEl = document.getElementById('modal');
+
+loginBtn.addEventListener("click", () => {
+    modalEl.classList.toggle('show-modal');
+})
+
+closeBtn.addEventListener("click", () => {
+    modalEl.classList.remove('show-modal');
+})
