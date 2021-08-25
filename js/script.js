d9cab26e6b5169c0911195833430d5a695d72e16
Lavanya <lavanyask03@gmail.com>
2021-08-25 17:28:33 +0530

    adding carousel for student reviews

--- js/script.js
+++ js/script.js
@@ -36,3 +36,34 @@ categories.map((category) => {
     // append catEl to categoriesEl
     categoriesEl.appendChild(catEl);
 })
+
+// index page
+// student reviews carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
